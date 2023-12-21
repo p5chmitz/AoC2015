86594b0f210273712e8122a51a8c4b4d032f5232
p5chmitz <petermschmitz@gmail.com>
2023-12-20 21:52:14 -0800

    First day of Advent of Code completed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use std::fs::File;
+use std::io::Read;
+
+pub fn aoc1501_1() {
+    //Should result in 4
+    //let input = String::from("(()()(((()");
+    let mut input = String::new();
+
+    //Reads from file
+    match File::open("./input_files/aoc1501_1.txt") {
+        Ok(mut file) => {
+            match file.read_to_string(&mut input) {
+                Ok(_) => {
+                    //println!("File contents:\n{}", input);
+                }
+                Err(err) => {
+                    eprintln!("Error reading file: {}", err);
+                }
+            }
+        }
+        Err(err) => {
+            eprintln!("Error opening file: {}", err);
+        }
+    }
+
+    //Parses file and prints counter
+    let mut counter = 0;
+    let mut p = 1;
+    for character in input.chars() {
+        if character == '(' {
+            counter += 1;
+        }
+        if character == ')' {
+            counter -= 1;
+        }
+        if counter == -1 {
+            println!("The elevator goes into the basement on the {} iteration.", p);
+            break
+        }
+        p += 1;
+    }
+    println!("The resultant floor is: {}", counter);
+}
+
 fn main() {
-    println!("Hello, world!");
+    aoc1501_1();
 }
