be1e81fd831d66ce998649fc1cc7159af74a796c
p5chmitz <petermschmitz@gmail.com>
2024-02-16 23:08:46 -0800

    Updates main program

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,11 @@
-use std::fs::File;
-use std::io::Read;
-
-pub fn aoc1501_1() {
-    //Should result in 4
-    //let input = String::from("(()()(((()");
-    let mut input = String::new();
-
-    //Reads from file
-    match File::open("./input_files/aoc1501_1.txt") {
-        Ok(mut file) => {
-            match file.read_to_string(&mut input) {
-                Ok(_) => {
-                    //println!("File contents:\n{}", input);
-                }
-                Err(err) => {
-                    eprintln!("Error reading file: {}", err);
-                }
-            }
-        }
-        Err(err) => {
-            eprintln!("Error opening file: {}", err);
-        }
-    }
-
-    //Parses file and prints counter
-    let mut counter = 0;
-    let mut p = 1;
-    let mut cherry = false;
-    for character in input.chars() {
-        if character == '(' {
-            counter += 1;
-        }
-        if character == ')' {
-            counter -= 1;
-        }
-        if cherry == false {
-            if counter == -1 {
-                println!(
-                    "The elevator first goes into the basement on the {} iteration.",
-                    p
-                );
-                cherry = true;
-            }
-        }
-        p += 1;
-    }
-    println!("The resultant floor is: {}", counter);
-}
+mod day;
+use day::{one, two};
 
 fn main() {
-    aoc1501_1();
+    println!("Day 1:");
+    print!("\t");
+    one::runner();
+    println!("Day 2:");
+    let d2 = two::runner();
+    println!("\tArea: {}\n\tLength: {}", d2.0, d2.1);
 }
