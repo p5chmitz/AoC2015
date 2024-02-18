da149a7da217ee8437f5b062833cbb46d400d160
p5chmitz <petermschmitz@gmail.com>
2024-02-17 19:41:33 -0800

    Adds Day 3 to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 mod day;
-use day::{one, two};
+use day::{
+    one, 
+    two,
+    three,
+};
 
 fn main() {
     println!("Day 1:");
     print!("\t");
     one::runner();
+    
     println!("Day 2:");
     let d2 = two::runner();
-    println!("\tArea: {}\n\tLength: {}", d2.0, d2.1);
+    println!("\tWrapping paper area: {}\n\tTotal ribbon length: {}", d2.0, d2.1);
+
+    println!("Day 3:");
+    let d3 = three::runner();
+    println!("\tHouses that get at least one present: {}", d3.0);
+
 }
