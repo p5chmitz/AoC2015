2b59ef55fbd56cca92574ca0971c32f68faf3f56
p5chmitz <petermschmitz@gmail.com>
2024-02-18 15:26:41 -0800

    Adds intro printout to main.rs and reformats day prints

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,21 @@ use day::{
 };
 
 fn main() {
+    println!("My hacky attempts at Advent of Code™ 2015! 🎄🎁\nhttps://adventofcode.com/2015");
+
     println!("Day 1:");
-    print!("\t");
-    one::runner();
+    let d3 = one::runner();
+    println!("\tThe elevator first goes into the basement on the {} iteration.", d3.0);
+    println!("\tThe resultant floor is: {}", d3.1);
     
     println!("Day 2:");
     let d2 = two::runner();
-    println!("\tWrapping paper area: {}\n\tTotal ribbon length: {}", d2.0, d2.1);
+    println!("\tWrapping paper area: {}", d2.0);
+    println!("\tTotal ribbon length: {}", d2.1);
 
     println!("Day 3:");
     let d3 = three::runner();
     println!("\tHouses that get at least one present: {}", d3.0);
+    println!("\tUnique hits with Robo Santa™: {}", d3.1);
 
 }
